d9894719c499437e9357cac40b09d83629b71d00
Manish bhukar <125037273+manish-bhukar@users.noreply.github.com>
2024-03-09 11:00:39 +0530

    Login and signup authentication done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
-const express = require('express');
+import express from 'express';
+import dotenv from "dotenv";
+import cors from 'cors';
+import userRoute from "./Route/userRoute.js"
+import  connectdb  from './Database/db.js';
 const PORT=5000;
+dotenv.config();
 const app=express();
+
+app.use(express.json());
+connectdb();
+app.use(cors());
+app.use("/api/",userRoute);
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
