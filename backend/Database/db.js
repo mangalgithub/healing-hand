fcc608b2cebdba0f978adf740810289c9f6b161c
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2024-03-20 15:04:52 +0530

    added env file

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 // main().catch(err => console.log(err));
 
-const connectdb=async function main() {
-const db = await mongoose.connect(
-  "mongodb+srv://mangalkgupta004:qJYuW7Nk0QuOlp3T@cluster0.ltg6mvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
+const connectdb = async function main() {
+  const db = await mongoose.connect(process.env.MONGO_URI);
   console.log(`db connected ${db.connection.host}`);
-}
+};
 export default connectdb;
